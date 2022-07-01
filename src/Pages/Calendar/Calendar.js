38c9931b4d4bcdc498de74fd8f6161c60bdb9406
abdul awal suhail <abdullahsuhail43@gmail.com>
2022-07-02 04:54:31 +0600

    all update

--- src/Pages/Calendar/Calendar.js
+++ src/Pages/Calendar/Calendar.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
 import { format } from "date-fns";
-// import calender from "../../Assets/calender.png";
+import calender from "../../assets/calendar.png";
 
 const Calender = () => {
   const [date, setDate] = useState(new Date());
@@ -13,7 +13,7 @@ const Calender = () => {
   return (
     <div className="min-h-screen">
       <div
-        data-aos="zoom-out-down"
+        
         className="flex md:flex-row flex-col justify-center items-center md:gap-10 gap-4 lg:w-5/6 mx-auto"
       >
         <div
@@ -21,7 +21,7 @@ const Calender = () => {
           data-aos-offset="300"
           data-aos-easing="ease-in-sine"
         >
-          
+          <img className="w-full" style={{width:"385px"}} src={calender} alt="calender" />
         </div>
         <div
           data-aos="fade-left"
