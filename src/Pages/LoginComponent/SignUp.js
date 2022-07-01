aad9dc94d4ecc103447e80259a81edf632660954
abdul awal suhail <abdullahsuhail43@gmail.com>
2022-07-01 23:53:42 +0600

    loading added

---
+++ src/Pages/LoginComponent/SignUp.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
+import SocialLogin from '../SocialLogin/SocialLogin';
+import auth from '../../firebase.init';
+import './SignUp.css'
+import Loading from '../Loading/Loading';
+
+const SignUp = () => {
+    const [agree, setAgree] = useState(false);
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+    const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+
+    const navigate = useNavigate();
+
+    const navigateLogin = () => {
+        navigate('/login');
+    }
+
+    if (loading || updating) {
+        return <Loading></Loading>
+    }
+
+    if (user) {
+        console.log(user);
+    }
+
+    const handleRegister = async (event) => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+
+        await createUserWithEmailAndPassword(email, password);
+        await updateProfile({ displayName: name });
+        console.log('Updated profile');
+        navigate('/home');
+    }
+    return (
+        <div className='signup-form'>
+            <h2 style={{ textAlign: 'center' }}>Please Sign Up</h2>
+            <form onSubmit={handleRegister}>
+                <input type="text" name="name" id="" placeholder='Your Name' />
+
+                <input type="email" name="email" id="" placeholder='Email Address' required />
+
+                <input type="password" name="password" id="" placeholder='Password' required />
+                <input onClick={() => setAgree(!agree)} type="checkbox" name="terms" id="terms" />
+                <label className={`ps-2 ${agree ? '' : 'text-danger'}`} htmlFor="terms">Accept Terms and Conditions</label>
+                <input disabled={!agree} className='w-50 mx-auto btn btn-outline-secondary mt-2'
+                    type="submit"
+                    value="Sign Up" />
+            </form>
+            <p>Already have an account? <Link to="/login" className='text-success pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link> </p>
+            <SocialLogin></SocialLogin>
+        </div>
+    );
+};
+
+export default SignUp;
