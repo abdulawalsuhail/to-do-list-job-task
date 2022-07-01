27f62b1f79501c11a1c0b411660feba5754d6b2c
abdul awal suhail <abdullahsuhail43@gmail.com>
2022-07-01 23:46:28 +0600

    login component added

--- src/Pages/Navigation/Navigation.js
+++ src/Pages/Navigation/Navigation.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
     return (
         <>
             <Navbar bg="light" expand="lg">
                 <Container>
-                  
-                        <Navbar.Brand href="/">Task App</Navbar.Brand>
-                   
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    
-                        <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end'>
-                            <Nav>
-                                <Nav.Link className='text-dark' to="/todo">TO-DO</Nav.Link>
-                                <Nav.Link className='text-dark' to="/completedTask">Completed Tasks</Nav.Link>
-                                <Nav.Link className='text-dark' to="/calendar">Calendar</Nav.Link>
-                            </Nav>
-                        </Navbar.Collapse>
-                   
+
+                    <Navbar.Brand href="/">Task App</Navbar.Brand>
+
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+                    <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end'>
+                        <Nav>
+                            <Nav.Link className='text-dark'><Link className='text-dark' to="/todo">TO-DO</Link></Nav.Link>
+                            <Nav.Link className='text-dark'><Link className='text-dark' to="/completedTask">Completed Tasks</Link></Nav.Link>
+                            <Nav.Link className='text-dark' ><Link className='text-dark' to="/calendar">Calendar</Link></Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+
                 </Container>
             </Navbar>
         </>
